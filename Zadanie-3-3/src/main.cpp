4924f558be93d653503748ef00da9172bebb56a2
Piotr Jaworski <piotr.jaworski01@student.wat.edu.pl>
2021-01-30 10:15:13 +0100

    Wykonano zadanie dodatkowe 3.3.3 – ustawianie daty, operacje matematyczne

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -3,32 +3,19 @@
 #include <Wire.h>
 
 LiquidCrystal lcd (12, 11, 5, 4, 3, 2);
-char tablica[]="Piotr";
-  int tabLen  = 0;
-void lcdClear(void);
+char rxBuff[32];
+  int ValueA = 0, ValueB = 0;
+  char operation [16];
+  
 void setup() {
 lcd.begin(16, 2);
   lcd.print("Piotr Jaworski");
-  tabLen = sizeof(tablica);
+  Serial.begin(9600);
   delay(2000);
 }
   void loop() {
-for (int i = 0; i<(18-tabLen); i++)
-{
-  lcdClear();
-  lcd.setCursor(i,0);
-  lcd.print(tablica);
-  delay(1000);
-}
-for (int i = (17-tabLen); i >= 0; i--) {
-  lcdClear();
-  lcd.setCursor(i, 1);
-  lcd.print(tablica);
-  delay(1000);
-}
-}
-void lcdClear(void)
-{
+Serial.readBytesUntil('\r', rxBuff, sizeof(rxBuff) - 1);
+
   lcd.setCursor(0,0);
   lcd.print("                ");
   lcd.setCursor(0,1);
